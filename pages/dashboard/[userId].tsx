71513134706e0189820b08bbaa0306642faf8983
Li Yu <gachuka@gmail.com>
2023-02-03 15:22:42 -0500

    Using id from url with useParam to find user, added dynamic role tags

--- pages/dashboard/[userId].tsx
+++ pages/dashboard/[userId].tsx
@@ -1,23 +1,39 @@
 import { createAvatar } from '@dicebear/core';
 import * as peeps from '@dicebear/open-peeps';
+import axios from 'axios';
 
-const username = "Schubert Kulminko"
-
-const mockUser = {
-  name: "Schubert Kulminko",
-  roles: [true, true, true, true, true, true, true]
-}
-
-const genAvatar = () => {
-  const avatar = createAvatar(peeps, {
-    seed: username,
-    flip: true,
-    translateX: 5,
-  });
-  return avatar.toDataUriSync();
-}
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react';
 
 export default function Dashboard() {
+
+  const [ name, setName ] = useState('')
+
+  const router = useRouter()
+  const userId = router.query.userId
+
+  const username = "Schubert Kulminko"
+
+  const mockUser = {
+    name: "Schubert Kulminko",
+    roles: [true, true, true, true, true, true, true]
+  }
+ 
+  useEffect(() => {
+    axios.get(`http://localhost:8080/dibs/${userId}`).then((res) => {
+      console.log(res.data)
+      setName(res.data.name)
+    })
+  },[])
+
+  const genAvatar = () => {
+    const avatar = createAvatar(peeps, {
+      seed: name,
+      flip: true,
+      translateX: 5,
+    });
+    return avatar.toDataUriSync();
+  }
 
   return (
     <div className="w-full max-w-[1280px]">
@@ -28,46 +44,58 @@ export default function Dashboard() {
           <img className='w-full object-cover rounded-full' src={genAvatar()} loading="lazy"/>
         </div>
         <div className='flex flex-col gap-4'>
-          <span className='text-4xl font-bold'>Hey, {username}</span>
+          <span className='text-4xl font-bold'>Hey, {name}</span>
           <p>How are you today? Ready to tackle some projects?</p>
         </div>
       </div>
 
-      <div className='flex flex-col mt-4 bg-emerald-500 rounded-3xl p-4'>
+      <div className='flex flex-col mt-4 bg-emerald-500 rounded-3xl p-4' id="roles-bar">
         <div className='flex gap-2'>
+          {mockUser.roles[0] ? 
           <div className='flex items-center gap-1 bg-emerald-900 w-fit py-1 px-3 rounded-md '>
             <div className='h-4 w-4 bg-purple-500 rounded-full'></div>
             <span className='font-bold text-white'>FullStack</span>
-          </div>
+          </div> 
+          :""}
+          {mockUser.roles[1] ? 
           <div className='flex items-center gap-1 bg-emerald-900 w-fit py-1 px-3 rounded-md'>
             <div className='h-4 w-4 bg-green-500 rounded-full'></div>
             <span className='font-bold text-white'>Front-End</span>
-          </div>
+          </div> 
+          :""}
+          {mockUser.roles[2] ? 
           <div className='flex items-center gap-1 bg-emerald-900 w-fit py-1 px-3 rounded-md'>
             <div className='h-4 w-4 bg-blue-500 rounded-full'></div>
             <span className='font-bold text-white'>Back-End</span>
-          </div>
+          </div> 
+          :""}
+          {mockUser.roles[3] ? 
           <div className='flex items-center gap-1 bg-emerald-900 w-fit py-1 px-3 rounded-md'>
             <div className='h-4 w-4 bg-yellow-500 rounded-full'></div>
             <span className='font-bold text-white'>PM</span>
           </div>
+          :""}
+          {mockUser.roles[4] ? 
           <div className='flex items-center gap-1 bg-emerald-900 w-fit py-1 px-3 rounded-md'>
             <div className='h-4 w-4 bg-red-500 rounded-full'></div>
             <span className='font-bold text-white'>QA Tester</span>
           </div>
+          :""}
+          {mockUser.roles[5] ? 
           <div className='flex items-center gap-1 bg-emerald-900 w-fit py-1 px-3 rounded-md'>
             <div className='h-4 w-4 bg-pink-500 rounded-full'></div>
             <span className='font-bold text-white'>UX/UI</span>
           </div>
+          :""}
+          {mockUser.roles[6] ? 
           <div className='flex items-center gap-1 bg-emerald-900 w-fit py-1 px-3 rounded-md'>
             <div className='h-4 w-4 bg-black rounded-full'></div>
             <span className='font-bold text-white'>DevOps</span>
-          </div>
+          </div> 
+          :""}
         </div>
       </div>
-
-      
-      
     </div>
   )
 }
+
